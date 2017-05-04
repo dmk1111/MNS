93c75ba88531d611114805e48957293e4aa94bb2
dmk1111 <dima.golysh@gmail.com>
2017-05-04 18:49:38 +0300

    added same style to staff page

--- src/home/staff/staffList.html.ts
+++ src/home/staff/staffList.html.ts
@@ -3,28 +3,33 @@ export const staffListHtml = `
   <div style="position: relative;" [ngBusy]="{busy: load, message: 'Завантаження'}"></div>
   <create-staff [regions]="regions" (onClose)="modalClose($event)" *ngIf="showCreate"></create-staff>
   
-  
-  <h3 >Персонал</h3>
-  <div (click)="goTo('menu')" style="cursor: pointer">Назад</div>
-   <div style="margin-bottom: 20px" >
-     <div style="text-align: right; margin-bottom: 5px" >
-                  <button md-mini-fab class="example-fab" (click)="openCreate()">
-                   <md-icon>add</md-icon>
-                  </button>
-      </div>
-    <datatable  *ngIf="staff && staff.length"
-      class="material"
-      [rows]="staff"
-      [columns]="column"
-      [columnMode]="'force'"
-      [headerHeight]="30"
-      [footerHeight]="50"
-      [rowHeight]="'auto'"
-               [selected]="selected"
-               [selectionType]="'cell'"
-               (select)="onSelect($event)"
-      [limit]="10">
-    </datatable>
+  <div class="staff_wrapper">
+    <div class="top">
+      <img class="logo" src="../../../img/logo.png">
+      <h3>Персонал</h3>
+    </div>
+    <button class="button-gradient" (click)="goTo('menu')">Назад</button>
+    <div class="add_material">
+                    <button md-fab class="example-fab" (click)="openCreate()">
+                     <md-icon>add</md-icon>
+                    </button>
+     </div>
+     <div class="inner-table" >
+       
+      <datatable  *ngIf="staff && staff.length"
+        class="material"
+        [rows]="staff"
+        [columns]="column"
+        [columnMode]="'force'"
+        [headerHeight]="30"
+        [footerHeight]="50"
+        [rowHeight]="'auto'"
+                 [selected]="selected"
+                 [selectionType]="'cell'"
+                 (select)="onSelect($event)"
+        [limit]="10">
+      </datatable>
+     </div>
    </div>
     <staffModal  *ngIf="open" (onClose)="closeHandle($event)"></staffModal>
     
